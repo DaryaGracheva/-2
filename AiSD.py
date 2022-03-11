e56d847ec35d37b1cee888922a30974541f560c0
DaryaGracheva <99476603+DaryaGracheva@users.noreply.github.com>
2022-03-12 00:03:03 +0400

    Update AiSD.py

--- AiSD.py
+++ AiSD.py
@@ -1,19 +1,19 @@
 import re
-with open("text2.txt", "r") as file:
+with open("text.txt", "r") as file:
         s = re.sub(r'\s+', ' ', file.read().decode('UTF-8'))
 
-print("Предложения, не имеющие запятых:")        
+print("РџСЂРµРґР»РѕР¶РµРЅРёСЏ, РЅРµ РёРјРµСЋС‰РёРµ Р·Р°РїСЏС‚С‹С…:")        
 print(" ")
 
 flag = 0
-for s in re.split(r'(?<=[.!?…\n\r\t]) ', s):
+for s in re.split(r'(?<=[.!?вЂ¦\n\r\t]) ', s):
         element = re.split(",", s)
         if len(element) < 2:
                 flag += 1
                 print(s)
         
 if flag == 0:
-        print ("Таких предложений в тексте нет")
+        print ("РўР°РєРёС… РїСЂРµРґР»РѕР¶РµРЅРёР№ РІ С‚РµРєСЃС‚Рµ РЅРµС‚")
 
                        
         
